82f3034b2b78ce88a2334d9eaa8f85dbf629796d
jenz <jenz@silpion.de>
2016-04-11 14:17:39 +0200

    added compression

--- dumpDB.sh
+++ dumpDB.sh
@@ -10,11 +10,11 @@ eval $(parse_yaml app/config/parameters.yml config_)
 
 # access yaml content
 
-filename="~/mysql_dumps/${config_parameters_database_name}_`date +"%s"`.sql"
+filename="~/mysql_dumps/${config_parameters_database_name}_`date +"%s"`.sql.gz"
 echo "Dumping Database ${config_parameters_database_name} to ${filename} ."
 #mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > "./mysql_dumps/"${config_parameters_database_name}"_`date +"%s"`.sql"
 
-mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > $filename
+mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" | gzip -9 > "${filename}"
 res=$?
 echo "Finished with Code $res"
 exit $res
