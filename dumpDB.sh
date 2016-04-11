341fbf5aa0efac8f3ac1542a1e090f3bf0a8a4c6
jenz <jenz@silpion.de>
2016-04-11 14:03:28 +0200

    updated Script

--- dumpDB.sh
+++ dumpDB.sh
@@ -12,7 +12,9 @@ eval $(parse_yaml app/config/parameters.yml config_)
 
 filename="~/mysql_dumps/${config_parameters_database_name}_`date +"%s"`.sql"
 echo "Dumping Database ${config_parameters_database_name} to ${filename}".
-mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > "./mysql_dumps/"${config_parameters_database_name}"_`date +"%s"`.sql"
+#mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > "./mysql_dumps/"${config_parameters_database_name}"_`date +"%s"`.sql"
+
+mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > ~/mysql_dumps/"$config_parameters_database_name"_`date +"%s"`.sql
 res=$?
 echo "Finished with Code $res"
 exit $res
