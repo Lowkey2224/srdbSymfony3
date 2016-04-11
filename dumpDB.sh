b9d2446cbdb4e5c230b4d9d1943e1c87a4cb3bfa
jenz <jenz@silpion.de>
2016-04-11 10:15:30 +0200

    Added Script to backup SQL Database

---
+++ dumpDB.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#!/bin/sh
+
+# include parse_yaml function
+. parse_yaml.sh
+
+# read yaml file
+eval $(parse_yaml app/config/parameters.yml config_)
+
+# access yaml content
+
+filename="~/mysql_dumps/${config_parameters_database_name}_`date +"%s"`.sql"
+echo "Dumping Database ${config_parameters_database_name} to ${filename}".
+mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" > ~/mysql_dumps/${config_parameters_database_name}_`date +"%s"`.sql
+res=$?
+echo "Finished with Code $res"
+exit $res
