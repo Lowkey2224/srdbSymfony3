c44675dad8cfea30a5f486c5f8eeba448ed1e173
jenz <jenz@silpion.de>
2016-04-11 14:40:57 +0200

    added date to filename

--- dumpDB.sh
+++ dumpDB.sh
@@ -10,8 +10,8 @@ eval $(parse_yaml app/config/parameters.yml config_)
 
 # access yaml content
 
-echo "Dumping Database ${config_parameters_database_name} to /home/mysql_dump/${config_parameters_database_name}.sql.gz ."
-mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" | gzip -9 > /home/mysql_dump/${config_parameters_database_name}.sql.gz
+echo "Dumping Database ${config_parameters_database_name} to /home/mysql_dump/${config_parameters_database_name}`date +"%s"`.sql.gz ."
+mysqldump -u "$config_parameters_database_user" --password="$config_parameters_database_password" "$config_parameters_database_name" | gzip -9 > /home/mysql_dump/${config_parameters_database_name}`date +"%s"`.sql.gz
 res=$?
 echo "Finished with Code $res"
 exit $res
