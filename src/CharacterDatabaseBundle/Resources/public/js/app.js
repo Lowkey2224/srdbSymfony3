0cdd9c2f2cb4d2c148572040ffaaf1f71ce46788
jenz <jenz@silpion.de>
2016-04-26 12:50:44 +0200

    added directive login-register which renders as a menu in small mode, and 2 Nav Items in biggermodes

--- src/CharacterDatabaseBundle/Resources/public/js/app.js
+++ src/CharacterDatabaseBundle/Resources/public/js/app.js
@@ -24,48 +24,56 @@
         });
     }]);
 
+    app.directive('loginRegister', function () {
+        return {
+            restrict: 'E',
+            templateUrl: app.bundleDir + 'html/directives/login-register.html',
+        };
+    });
+
+
     app.config(function ($routeProvider) {
 
-        $routeProvider.when('/', {templateUrl: app.bundleDir+'html/home.html', reloadOnSearch: false});
-        $routeProvider.when('/new', {templateUrl: app.bundleDir+'html/scroll.html', reloadOnSearch: false});
+        $routeProvider.when('/', {templateUrl: app.bundleDir + 'html/home.html', reloadOnSearch: false});
+        $routeProvider.when('/new', {templateUrl: app.bundleDir + 'html/scroll.html', reloadOnSearch: false});
         $routeProvider.when('/mine', {
-            templateUrl: app.bundleDir+'html/toggle.html',
+            templateUrl: app.bundleDir + 'html/toggle.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/character/:characterId', {
-            templateUrl: app.bundleDir+'html/character-show.html',
+            templateUrl: app.bundleDir + 'html/character-show.html',
             //reloadOnSearch: false
         });
         $routeProvider.when('/all', {
-            templateUrl: app.bundleDir+'html/characters.html',
+            templateUrl: app.bundleDir + 'html/characters.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/skills', {
-            templateUrl: app.bundleDir+'html/accordion.html',
+            templateUrl: app.bundleDir + 'html/accordion.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/specs', {
-            templateUrl: app.bundleDir+'html/overlay.html',
+            templateUrl: app.bundleDir + 'html/overlay.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/attributes', {
-            templateUrl: app.bundleDir+'html/forms.html',
+            templateUrl: app.bundleDir + 'html/forms.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/traditions', {
-            templateUrl: app.bundleDir+'html/dropdown.html',
+            templateUrl: app.bundleDir + 'html/dropdown.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/totems', {
-            templateUrl: app.bundleDir+'html/touch.html',
+            templateUrl: app.bundleDir + 'html/touch.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/cyberware', {
-            templateUrl: app.bundleDir+'html/swipe.html',
+            templateUrl: app.bundleDir + 'html/swipe.html',
             reloadOnSearch: false
         });
         $routeProvider.when('/logout', {
-            templateUrl: app.bundleDir+'html/drag.html',
+            templateUrl: app.bundleDir + 'html/drag.html',
             reloadOnSearch: false
         }).otherwise({
             redirectTo: '/all'
