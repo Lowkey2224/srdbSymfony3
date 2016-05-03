79dc7b6c0dd64305a1b58eeeba9731737f644e3a
jenz <jenz@silpion.de>
2016-05-03 09:41:37 +0200

    added frontend form to create Character, corrected Service in order to respons to the complexer JSON of the Form

--- allowedIPs.php
+++ allowedIPs.php
@@ -7,4 +7,7 @@ $allowedIps = [
     '127.0.0.1',
     'fe80::1',
     '::1',
+    '213.209.99.169', // Silpion Offics
+    '213.209.99.170', // Silpion Offics WiFi
+    '31.18.158.60', // Loki@Home
 ];
